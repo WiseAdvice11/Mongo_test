cb41bfcc41ae0e60a48738c90428c1b69add090d
WiseAdvice11 <breweroff@gmail.com>
2022-02-20 00:40:38 +0300

    fill the YAML file

---
+++ Mongo_test/src/main/java/ru/a1/Mongo_test/MongoTestApplication.java
@@ -0,0 +1,13 @@
+package ru.a1.Mongo_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoTestApplication.class, args);
+	}
+
+}
